b07f5b2ae8ddc2087069e2acfe5c673056379de8
Mark Tertishniy <mtertishniy@gmail.com>
2013-04-09 10:51:35 +1100

    Some changes in index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+
+	//Модель REST
+	//angular.js
 	error_reporting(E_ALL);
 
 	require_once('includes/settings.php');
@@ -7,17 +10,15 @@
 	$smarty = new Smarty_Musshop();
 
 	$req = explode('/', substr($_SERVER['REQUEST_URI'], 1));
-	switch ($req[0])
-	{
+	switch ($req[0]) {
 		case '': case null: case false:
-			$smarty->assign('title', 'Musstock - good shop!');
+			$smarty->assign('title', 'GIIIIIIITTT!!');
 			$smarty->assign('name', 'Katty');
 			$smarty->assign('caption', 'Добро пожаловать в магазин, котятки');
 			$smarty->assign('container', Array(0 => 'main.tpl'));
 			break;
 		case 'admin':
-			switch ($req[1])
-			{
+			switch ($req[1]) {
 				case 'products':
 					$smarty->assign('sb_components', Array(0 => 'left_menu.tpl'));
 					include_once('includes/products.php');
